abaed51094529ff4dfa7e66d0ccbe3869c82ca4a
Angel Carballeira <angelcarballeira@gmail.com>
2023-11-22 10:08:05 -0300

    start project

--- desde-cero/cratest/src/App.js
+++ desde-cero/cratest/src/App.js
@@ -1,4 +1,4 @@
-import logo from './platzi.webp';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://platzi.com/reactjs"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
